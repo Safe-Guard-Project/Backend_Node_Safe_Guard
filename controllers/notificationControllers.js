d0bd5851751cf9ddd2a6b5155dad52ab497dbad3
Ekbel <zrellliekbel@gmail.com$>
2023-11-06 20:07:29 +0100

    cmntcontrollers

---
+++ controllers/notificationControllers.js
@@ -0,0 +1,27 @@
+
+getAllnotif, getOnceNotif, deleteOnceNotif, deleteAllNotif
+
+import notification from '../models/notification.js';
+
+
+export function getAllnotif(req, res) {
+    notification.find({})
+      .then((docs) => {
+        res.status(200).json(docs);
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err });
+      });
+  }
+
+
+  export function getOnceNotif(req, res) {
+    
+    notification.findById({_id : req.params._id})
+      .then((docs) => {
+        res.status(200).json(docs);
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err });
+      });
+  }
