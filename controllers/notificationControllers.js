0ef8a4a7c617c3a175acaac4190810b8c6ce226c
Ekbel <zrellliekbel@gmail.com$>
2023-11-06 20:10:19 +0100

    notifcontrollers

--- controllers/notificationControllers.js
+++ controllers/notificationControllers.js
@@ -1,7 +1,7 @@
 
-getAllnotif, getOnceNotif, deleteOnceNotif, deleteAllNotif
 
 import notification from '../models/notification.js';
+
 
 
 export function getAllnotif(req, res) {
@@ -25,3 +25,23 @@ export function getAllnotif(req, res) {
         res.status(500).json({ error: err });
       });
     }
+
+    export function deleteAllNotif(req, res) {
+        CommentairesInformation.deleteMany({})
+          .then((docs) => {
+            res.status(200).json(docs);
+          })
+          .catch((err) => {
+            res.status(500).json({ error: err });
+          });
+      }
+      
+      export function deleteOnceNotif(req, res) {
+        commentairesInformation.findOneAndDelete({ _id: req.params._id })
+          .then((doc) => {
+            res.status(200).json(doc);
+          })
+          .catch((err) => {
+            res.status(500).json({ error: err });
+          });
+      }
